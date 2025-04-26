411ef49f8bf23231184f0fbaf0178081598e7693
yu <yul1024@outlook.com>
2025-04-26 16:19:15 +0800

    Create overall_workflow.py

---
+++ mas/workflow/overall_workflow.py
@@ -0,0 +1,20 @@
+"""
+整个MAS的workflow。
+"""
+
+from llama_index.core.workflow import (
+    StartEvent,
+    StopEvent,
+    Workflow,
+    step,
+    Event,
+    Context
+)
+
+
+# class MainWorkflow(Workflow):
+#     @step
+
+
+if __name__ == '__main__':
+    pass
