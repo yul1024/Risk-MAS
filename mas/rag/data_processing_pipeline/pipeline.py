396ba9d7a7a58c232c4262f6e482aac474c36b36
yu <yul1024@outlook.com>
2025-04-29 10:17:32 +0800

    Update pipeline.py

--- mas/rag/data_processing_pipeline/pipeline.py
+++ mas/rag/data_processing_pipeline/pipeline.py
@@ -1,6 +1,18 @@
 """
-集成
+集成所有构建外部可索引知识库的操作。
 """
+
+from llama_index.core.ingestion import IngestionPipeline
+
+
+class PipelineFactory:
+    def __init__(self):
+        ...
+
+    def get_default_pipeline(self):
+        pipeline = IngestionPipeline(
+            transformations=[],
+        )
 
 
 if __name__ == '__main__':
