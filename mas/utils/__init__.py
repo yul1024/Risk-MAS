833522d053646cc91b4c8cf49e10824ab7057ecc
yu <yul1024@outlook.com>
2025-04-29 10:17:30 +0800

    Update __init__.py

--- mas/utils/__init__.py
+++ mas/utils/__init__.py
@@ -1,11 +1,13 @@
 """
-
+所有package都可能会用到的工具。
 """
 
 # __all__ = [
 #     'LLMFactory',
 #     'EmbeddingModelFactory',
 # ]
+
+from .load_prompt_template import load_prompt_template
 
 from .llm_factory import LLMFactory
 from .embedding_model_factory import EmbeddingModelFactory
