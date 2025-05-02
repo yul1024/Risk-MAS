34b35794de52b7763de97a2ba2e08b62410eaf77
yu <yul1024@outlook.com>
2025-05-02 21:01:57 +0800

    Update test_model_client.py

--- tests/unit_tests/test_model_client.py
+++ tests/unit_tests/test_model_client.py
@@ -2,11 +2,17 @@
 测试LLM和embedding_model是否可以正常请求。
 """
 
-from mas.utils import LLMFactory, EmbeddingModelFactory
+from mas.utils import LLMFactory, VLMFactory, EmbeddingModelFactory
 
 
 def test_llm():
     llm_factory = LLMFactory()
+    llm = llm_factory.get_qwen_llm()
+
+
+def test_vlm():
+    vlm_factory = VLMFactory()
+    vlm = vlm_factory.get_qwen_vlm()
 
 
 def test_embedding_model():
