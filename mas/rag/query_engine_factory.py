79b2287f7d6eb3f8345f81a23961f22c80387384
yu <yul1024@outlook.com>
2025-05-04 10:20:26 +0800

    Create query_engine_factory.py

---
+++ mas/rag/query_engine_factory.py
@@ -0,0 +1,44 @@
+"""
+基于llama-index构建的query engine。
+"""
+
+from llama_index.core.tools import QueryEngineTool
+from llama_index.core.base.base_query_engine import BaseQueryEngine
+
+from llama_index.core.query_engine import RouterQueryEngine
+
+
+class QueryEngineFactory:
+    def __init__(
+        self,
+        index,
+    ):
+        self.index = index
+
+    def get_query_engine(self):
+        return self.index.as_query_engine()
+
+
+def get_query_engine_tool(
+    query_engine: BaseQueryEngine,
+    description: str
+) -> QueryEngineTool:
+    """
+    将构建的query_engine转换为所有agent可以使用的tool。
+
+    Args:
+        query_engine: 通过RAG部分构建的query-engine。
+        description: 工具的描述。用于异构数据路由使用。
+
+    Returns:
+        已经处理过的tool。
+    """
+    tool = QueryEngineTool.from_defaults(
+        query_engine=query_engine,
+        description=description,
+    )
+    return tool
+
+
+if __name__ == '__main__':
+    pass
