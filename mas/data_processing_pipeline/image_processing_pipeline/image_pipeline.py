427e660db9dd35d68a2e937db9add6e05688d651
yu <yul1024@outlook.com>
2025-05-04 10:20:41 +0800

    Create image_pipeline.py

---
+++ mas/data_processing_pipeline/image_processing_pipeline/image_pipeline.py
@@ -0,0 +1,25 @@
+
+
+from pathlib import Path
+
+
+class ImagePipeline:
+    def __init__(self):
+        ...
+
+    def run(self):
+        ...
+
+    def load_image(self, pdf_dir: Path):
+        """
+        从已经
+
+        Args:
+            pdf_dir:
+
+        Returns:
+
+        """
+
+if __name__ == '__main__':
+    pass
