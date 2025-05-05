86fd28a21e1e0beef4d39c3a26362f7775c6cd17
yu <yul1024@outlook.com>
2025-05-05 21:30:02 +0800

    Update global_config.py

--- mas/global_config.py
+++ mas/global_config.py
@@ -1,10 +1,22 @@
 
 # pdf
+TEXT_LOADING_MODE = ['rule', 'ocr', 'vlm']
+TEXT_PARSING_MODE = ['markdown', ]
 
 
 # milvus
+URI = "http://localhost:19530"
 
 
 # IngestionPipeline cache
-
+PIPELINE_CACHE=dict(
+    text=dict(
+        parsing=[],
+        embedding=[],
+    ),
+    image=dict(
+        parsing=[],
+        embedding=[],
+    )
+)
 
