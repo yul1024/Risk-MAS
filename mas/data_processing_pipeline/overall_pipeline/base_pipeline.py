2daf31e77f78942d67a1f350a731250d25ed9859
yu <yul1024@outlook.com>
2025-05-05 21:30:10 +0800

    Create base_pipeline.py

---
+++ mas/data_processing_pipeline/overall_pipeline/base_pipeline.py
@@ -0,0 +1,50 @@
+"""
+完整pipeline基础功能。
+
+实现:
+    - 文档加载。
+    - 文档存储。
+"""
+
+from mas.utils import DocumentStoreManager
+from mas.data_processing_pipeline.text_loading_pipeline import TextLoadingPipeline
+from mas.data_processing_pipeline.image_loading_pipeline import ImageLoadingPipeline
+
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core import Document
+
+from pathlib import Path
+
+
+class BasePipeline:
+    """
+
+    """
+    def __init__(
+        self,
+    ):
+        """"""
+
+        self.document_store_manager = DocumentStoreManager()
+
+    def load_documents_from_document_store(
+        self,
+        document_store_path: str | Path,
+    ) -> list[Document]:
+        """
+        从文档存储中加载文档。
+
+        使用我通用的document-store管理工具加载。
+
+        Args:
+            document_store_path:
+
+        Returns:
+
+        """
+        nodes = self.document_store_manager.load_nodes(document_store_path=document_store_path)
+        return nodes
+
+
+if __name__ == '__main__':
+    pass
