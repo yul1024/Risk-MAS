4d575f5d682f1e818ff587f764f57bf18adb4382
yu <yul1024@outlook.com>
2025-05-05 21:30:22 +0800

    Update __init__.py

--- mas/data_processing_pipeline/__init__.py
+++ mas/data_processing_pipeline/__init__.py
@@ -1,9 +1,16 @@
 """
 文件从原始的pdf格式，到可以接入RAG的vector-store。
 
-两个sub-pipeline:
-    - pdf --pipeline--> documents
-编码和存储:
-    - documents --embedding_model--> vector-store
+两个阶段:
+    加载:
+        - pdf --pipeline--> documents
+    编码和存储:
+        - documents --embedding_model--> vector-store
+
+为了加速，
+    - 文件预加载为document-store。
+    - 所有pipeline进行预构建，并持久化所有缓存。
 """
+
+from .pre_pdf_loader import PrePdfLoader
 
