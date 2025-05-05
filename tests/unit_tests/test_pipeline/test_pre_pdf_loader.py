842bd6743cd245b97e8b0310d3a2238f72750f21
yu <yul1024@outlook.com>
2025-05-05 21:29:23 +0800

    Create test_pre_pdf_loader.py

---
+++ tests/unit_tests/test_pipeline/test_pre_pdf_loader.py
@@ -0,0 +1,19 @@
+"""
+测试pdf预加载工具的运行。
+"""
+
+from mas.data_processing_pipeline import PrePdfLoader
+
+
+def test_pre_pdf_loader():
+    pre_pdf_loader = PrePdfLoader(
+        pdf_dir=r"D:\dataset\risk_mas_t\original_pdf",
+        base_image_pdf_dir=r"D:\dataset\risk_mas_t\image_pdf",
+        document_store_dir=r"D:\dataset\risk_mas_t\document_store",
+    )
+    pre_pdf_loader.batch_process_text(modes=['rule'])
+    pre_pdf_loader.batch_process_image(is_need_convert=True)
+
+
+if __name__ == '__main__':
+    test_pre_pdf_loader()
