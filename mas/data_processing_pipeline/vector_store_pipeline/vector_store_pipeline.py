ba1f6781f01c8616ef881227b95a1b10ec5c758e
yu <yul1024@outlook.com>
2025-05-06 21:31:42 +0800

    Create vector_store_pipeline.py

---
+++ mas/data_processing_pipeline/vector_store_pipeline/vector_store_pipeline.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+from llama_index.core.ingestion import IngestionPipeline
+
+
+class VectorStorePipeline:
+    def __init__(self):
+        ...
+
+
+if __name__ == '__main__':
+    pass
