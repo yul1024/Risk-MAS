b060d24e4925ac5cd1c0ebd35d663209b2d7d4ac
yu <yul1024@outlook.com>
2025-05-06 13:02:26 +0800

    Update global_config.py

--- mas/global_config.py
+++ mas/global_config.py
@@ -1,7 +1,17 @@
 
+# path
+BASE_PATH = rf""
+ORIGINAL_PDF_DIR = rf"{BASE_PATH}"
+BASE_IMAGE_PDF_DIR = rf"{BASE_PATH}"
+DOCUMENT_STORE_DIR = rf"{BASE_PATH}"
+PIPELINE_CACHE_DIR = rf"{BASE_PATH}"
+VECTOR_STORE_DIR = rf"{BASE_PATH}"
+
+
 # pdf
-TEXT_LOADING_MODE = ['rule', 'ocr', 'vlm']
+TEXT_LOADING_MODE = ['rule', 'ocr', 'vlm', ]
 TEXT_PARSING_MODE = ['markdown', ]
+IMAGE_PARSING_MODE = ['none', ]
 
 
 # milvus
@@ -11,12 +21,12 @@ URI = "http://localhost:19530"
 # IngestionPipeline cache
 PIPELINE_CACHE=dict(
     text=dict(
-        parsing=[],
-        embedding=[],
+        parsing=['markdown'],
+        embedding=['', ],
     ),
     image=dict(
-        parsing=[],
-        embedding=[],
+        parsing=['none'],
+        embedding=['', ],
     )
 )
 
