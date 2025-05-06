d0da6961a114eab4356c57afd5917c097f9e231d
yu <yul1024@outlook.com>
2025-05-06 21:31:37 +0800

    Update __init__.py

--- mas/models/__init__.py
+++ mas/models/__init__.py
@@ -5,5 +5,7 @@
     - 多模态的支持很少。需要自主实现multi-modal embedding model。
 """
 
-
+from .embedding_model_factory import EmbeddingModelFactory
+from .llm_factory import LLMFactory
+from .vlm_factory import VLMFactory
 
