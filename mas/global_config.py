df103c5e3d22b1a2112a2d5bbaef12deb5b9abb0
yu <yul1024@outlook.com>
2025-05-06 21:31:39 +0800

    Update global_config.py

--- mas/global_config.py
+++ mas/global_config.py
@@ -1,11 +1,11 @@
 
 # path
 BASE_PATH = rf""
-ORIGINAL_PDF_DIR = rf"{BASE_PATH}"
-BASE_IMAGE_PDF_DIR = rf"{BASE_PATH}"
-DOCUMENT_STORE_DIR = rf"{BASE_PATH}"
-PIPELINE_CACHE_DIR = rf"{BASE_PATH}"
-VECTOR_STORE_DIR = rf"{BASE_PATH}"
+ORIGINAL_PDF_DIR = rf"{BASE_PATH}/original_pdf"
+BASE_IMAGE_PDF_DIR = rf"{BASE_PATH}/image_pdf"
+DOCUMENT_STORE_DIR = rf"{BASE_PATH}/document_store"
+PIPELINE_CACHE_DIR = rf"{BASE_PATH}/pipeline_cache"
+VECTOR_STORE_DIR = rf"{BASE_PATH}/vector_store"
 
 
 # pdf
@@ -19,14 +19,30 @@ URI = "http://localhost:19530"
 
 
 # IngestionPipeline cache
-PIPELINE_CACHE=dict(
-    text=dict(
-        parsing=['markdown'],
-        embedding=['', ],
-    ),
-    image=dict(
-        parsing=['none'],
-        embedding=['', ],
-    )
+# PIPELINE_CACHE=dict(
+#     text=dict(
+#         parsing=['markdown'],
+#         embedding=['', ],
+#     ),
+#     image=dict(
+#         parsing=['none'],
+#         embedding=['', ],
+#     )
+# )
+
+PIPELINE_CACHE_CONTENT = dict(
+    parsing=['markdown', ],
+    embedding=['model_1', 'model_2', 'model_3', 'model_4', ]
 )
+
+
+# vector store pipeline
+VECTOR_STORE_PIPELINE = [
+    ['text', 'rule', 'model_1'],
+    ['text', 'ocr', 'model_1'],
+    ['text', 'vlm', 'model_1'],
+    ['image', 'none', 'model_2'],
+    ['image', 'none', 'model_3'],
+    ['image', 'none', 'model_4'],
+]
 
