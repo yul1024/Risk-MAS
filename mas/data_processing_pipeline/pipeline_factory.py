97e5aa5ba2f81ece5e107e53f422f5e5fe80d5d2
yu <yul1024@outlook.com>
2025-05-06 21:32:11 +0800

    Update pipeline_factory.py

--- mas/data_processing_pipeline/pipeline_factory.py
+++ mas/data_processing_pipeline/pipeline_factory.py
@@ -4,35 +4,13 @@
 使用llama-index内置的IngestionPipeline，使用缓存以加速和节省资源。
 """
 
+from mas.global_config import PIPELINE_CACHE_CONTENT
+
 from llama_index.core.ingestion import IngestionPipeline
 
 
 class PipelineFactory:
     def __init__(self):
-        ...
-
-    def get_text_only_pipeline(self):
-        """
-        仅提取pdf中文本的部分。
-
-        Returns:
-
-        """
-
-    def get_ocr_pipeline(self):
-        """
-        使用ocr工具，提取pdf中布局的信息。
-
-        Returns:
-
-        """
-        # return DocumentPipeline(
-        #     node_parser=MarkdownNodeParser(),
-        #     embedding_model=,
-        #     vector_store=,
-        # )
-
-    def get_multi_modal_pipeline(self):
         ...
 
 
